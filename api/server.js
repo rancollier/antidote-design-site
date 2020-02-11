70355a15a2e59dfca11496914454b06348113f3e
Randy Collier <randycollier.tx@gmail.com>
2020-02-10 22:44:44 -0600

    first commit

---
+++ api/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3033
+
+app.get('/', (req, res) => res.send(`Antidote ${port}`))
+app.get('/api', (req, res) => res.send('Api yes12'))
+app.get('/api/a', (req, res) => res.send('Api one'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
