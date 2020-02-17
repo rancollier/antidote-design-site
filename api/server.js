11107c237bbe298bf109c24d064fb7fbfd6323a8
Randy Collier <randycollier.tx@gmail.com>
2020-02-17 14:12:28 -0600

    update nginx

--- api/server.js
+++ api/server.js
@@ -3,6 +3,6 @@ const app = express()
 const port = process.env.PORT || 3033
 
 app.get('/', (req, res) => res.send(`Antidote ${port}`))
-app.get('/api', (req, res) => res.send('Api yes12'))
+app.get('/api', (req, res) => res.send('Api yes123'))
 app.get('/api/a', (req, res) => res.send('Api one'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
