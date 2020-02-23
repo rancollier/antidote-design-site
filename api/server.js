46a9ca44b1026676145b2ba4a0fcfa12e606918f
Randy Collier <randycollier.tx@gmail.com>
2020-02-22 21:56:50 -0600

    update docker

--- api/server.js
+++ api/server.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
+const router = express.Router();
 const port = process.env.PORT || 3033
+const path = __dirname + '/views/';
+router.get('/',function(req,res){
+    res.sendFile(path + 'index.html');
+  });
+  router.get('/api', (req, res) => res.send('Api yes12'))
+  router.get('/api/a', (req, res) => res.send('Api one'))
 
-app.get('/', (req, res) => res.send(`Antidote ${port}`))
-app.get('/api', (req, res) => res.send('Api yes12'))
-app.get('/api/a', (req, res) => res.send('Api one'))
+app.use(express.static(path));
+app.use('/', router);
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
