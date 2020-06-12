2a65db8f9ee6522e4854f0d5131cd156f9f424ef
Randy Collier <randycollier.tx@gmail.com>
2020-06-12 18:10:04 -0500

    update text

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -25,7 +25,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Randy Collier
+          Front-End Engineer
         </a>
       </header>
     </div>
